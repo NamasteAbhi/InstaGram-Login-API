2d00f2d68aba0070715cffea56e5544e8af215ae
Abhinav Bhardwaj <95855028+NamasteAbhi@users.noreply.github.com>
2022-12-12 00:33:03 +0530

    Add files via upload

---
+++ example.py
@@ -0,0 +1,11 @@
+from NamasteiG import *
+UserName=""
+PassWord=""
+Data=Instagram.Login(UserName,PassWord)['Response'].text #PRINT DATA IN TEXT
+print(Data)
+
+Data=Instagram.Login(UserName,PassWord)['Response'].headers #PRINT DATA IN HEADERS
+print(Data)
+
+Data=Instagram.Login(UserName,PassWord) #PRINT ALL DATA
+print(Data)
